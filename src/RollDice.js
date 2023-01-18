92649fbc9388676ebee207c408964676b53d9239
David Webb <webbd5050@gmail.com>
2023-01-18 14:07:53 -0500

    disabled button during rolling

--- src/RollDice.js
+++ src/RollDice.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import Die from "./Die"
+import "./RollDice.css"
 
 export default class RollDice extends Component {
   static defaultProps = {
@@ -12,6 +13,7 @@ export default class RollDice extends Component {
     this.state = {
       die1: "one",
       die2: "one",
+      rolling: false,
     }
     // we want this to be bound to the instance of the component
     this.roll = this.roll.bind(this)
@@ -22,15 +24,26 @@ export default class RollDice extends Component {
     const newDie1 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)]
     const newDie2 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)]
     // set State with new rolls
-    this.setState({ die1: newDie1, die2: newDie2 })
+    this.setState({ die1: newDie1, die2: newDie2, rolling: true })
+
+    //Wait 1 second, then set rolling to false
+    setTimeout(() => {
+      this.setState({ rolling: false })
+    }, 1000)
   }
 
   render() {
     return (
-      <div>
-        <Die face={this.state.die1} />
-        <Die face={this.state.die2} />
-        <button onClick={this.roll}>Roll Dice</button>
+      <div className="RollDice">
+        <div className="RollDice-container">
+          <Die face={this.state.die1} />
+          <Die face={this.state.die2} />
+        </div>
+        <div>
+          <button onClick={this.roll} disabled={this.state.rolling}>
+            {this.state.rolling ? "Rolling..." : "Role Dice!"}
+          </button>
+        </div>
       </div>
     )
   }
